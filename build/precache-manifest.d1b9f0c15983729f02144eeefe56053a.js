ad5bda1f5c3770357105d2f6017397684a4535a1
Arnab Mallick <49829856+peple-choice@users.noreply.github.com>
2019-12-30 00:33:33 +0530

    Upload Build Folder
    Upload Build Folder

---
+++ build/precache-manifest.d1b9f0c15983729f02144eeefe56053a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db97a0dc94c8b953dd4c149c092e0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b0667feda9f707443a",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "f078ae40a861170cfc8d",
+    "url": "/static/js/2.818d79f2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.818d79f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "08b0667feda9f707443a",
+    "url": "/static/js/main.82a86558.chunk.js"
+  },
+  {
+    "revision": "c7bc9b3e778ee2dc375a",
+    "url": "/static/js/runtime-main.b5215c19.js"
+  }
+]);
